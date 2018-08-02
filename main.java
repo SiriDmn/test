556477c05396755cba04f6ea26f017b03f87613e
siridmn <sirinapha.duangmanee@ktb.co.th>
2018-08-03 00:44:59 +0700

    Add click in main

--- main.java
+++ main.java
@@ -3,5 +3,5 @@ public static void Main(String [] args) { System.out.println("hello");
 System.out.println("hello");
 Home home = new Home();
 Menu menu = new Menu();
-
+Click click = new Click();
 }
